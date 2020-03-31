b21d5fb5d6dd90020c022afc524bb5ca16da90ca
capybara84 <eitoshi.ashihara@gmail.com>
2020-03-31 09:51:45 +0000

    Add print evaluation

--- wktk.ml
+++ wktk.ml
@@ -1,17 +1,44 @@
+open Syntax
+
+let rec read_print_loop verbose =
+    try
+        print_string "> ";
+        flush stdout;
+        let toks = Lexer.lexer "" @@ input_line stdin in
+        if verbose then print_endline @@ s_token_src_list toks;
+        let e = Parser.parse "" toks in
+        print_endline @@ "-> " ^ s_expr_src e;
+        print_endline @@ "-> " ^ s_expr e;
+        read_print_loop verbose
+    with
+        | Error (_, pos, msg) ->
+            Printf.printf "line=%d, col=%d: Error: %s\n" pos.line pos.col msg; read_print_loop verbose
+        | Sys_error s -> print_endline s
+        | End_of_file -> ()
 
 let () =
-    let filenames = ref [] in
-    let verbose = ref false in
-    let quit = ref false in
+    let filenames = ref []
+    and verbose = ref false
+    and do_test = ref false
+    and do_test_print = ref false
+    and do_print = ref false
+    in
     Arg.parse
         [
             ("-v", Arg.Unit (fun () -> verbose := true), " verbose");
-            ("-t", Arg.Unit (fun () -> quit := true; Test.test !verbose), " unit test");
-            ("-tp", Arg.Unit (fun () -> quit := true; Test.test_print !verbose), " test print");
+            ("-t", Arg.Unit (fun () -> do_test := true), " unit test");
+            ("-tp", Arg.Unit (fun () -> do_test_print := true), " test print");
+            ("-p", Arg.Unit (fun () ->  do_print := true), " print expression");
         ]
         (fun name -> filenames := name :: !filenames)
-        "usage: wktk [-v]][-p][-t][-tp] filename...";
-    if not !quit then
-        List.iter (fun x -> print_endline x) !filenames;
-    ()
+        "usage: wktk [-v]][-t][-tp][-p] filename...";
+    List.iter (fun x -> print_endline x) !filenames;
+    if !do_test then
+        Test.test !verbose
+    else if !do_test_print then
+        Test.test_print !verbose
+    else if !do_print then
+        read_print_loop !verbose
+    else
+        ()
 
